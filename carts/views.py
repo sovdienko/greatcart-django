e3e5af1c7da8e6b08b8b9b3cceb4d623d74618e6
sovdienko <sovdienko@gmail.com>
2023-02-15 22:43:43 +0200

    Add carts

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from store.models import Product
+
+from .models import Cart, CartItem
 
 
 # Create your views here.
-def cart(request):
-    return render(request=request, template_name="store/cart.html")
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def _get_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request=request))
+    product = get_object_or_404(Product, id=product_id)
+    return CartItem.objects.get(product=product, cart=cart)
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)  # get the product
+    try:
+        cart = Cart.objects.get(
+            cart_id=_cart_id(request=request)
+        )  # get the cart using the cart_id presents in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request=request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1  # increment quantity of Product in the cart
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+    cart_item.save()
+    return redirect("cart")
+
+
+def remove_cart(request, product_id):
+    cart_item = _get_cart_item(request=request, product_id=product_id)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect("cart")
+
+
+def remove_cart_item(request, product_id):
+    cart_item = _get_cart_item(request=request, product_id=product_id)
+    cart_item.delete()
+    return redirect("cart")
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request=request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
+    except ObjectNotExist:
+        grand_total = 0
+        tax = 0
+    context = {
+        "total": total,
+        "quantity": quantity,
+        "tax": tax,
+        "grand_total": grand_total,
+        "cart_items": cart_items,
+    }
+    return render(request=request, template_name="store/cart.html", context=context)
