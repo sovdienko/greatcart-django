45b4ae33c9b55fbba01af1e3f422bf55e7800f18
sovdienko <sovdienko@gmail.com>
2023-03-01 19:30:01 +0200

    eb

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = ["greatcart-env.eba-pcdnd9gp.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "greatcart-env.eba-vm6pie7t.us-west-2.elasticbeanstalk.com",
+]
 
 
 # Application definition
